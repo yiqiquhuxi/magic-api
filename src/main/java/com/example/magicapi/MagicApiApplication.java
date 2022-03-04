2cbe6d4704111129069a724eea7aa7322af65721
yiqiquhuxi <302616601@qq.com>
2022-03-04 18:58:56 +0800

    init
    init

---
+++ src/main/java/com/example/magicapi/MagicApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.magicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicApiApplication.class, args);
+    }
+
+}
